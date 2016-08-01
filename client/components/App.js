3f017fffe84f768a5e869de0ffb4bae5214d1bf1
alexpurhalo <alexpurhalo@gmail.com>
2016-08-01 18:43:42 +0300

    first-component-rendering-with-webpack

---
+++ client/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+	render() {
+		return (
+			<div>
+				<h1> Notes !!</h1>
+			</div>
+		);
+	}
+});
+
+export default App;
---
+++ client/main.js
@@ -0,0 +1,11 @@
+// React modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Application's components
+import App from './components/App';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('mount-point')
+);
