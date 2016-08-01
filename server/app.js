2ee0dd6f1fc858faa24d0f3d75ab2f5ce0bf6607
alexpurhalo <alexpurhalo@gmail.com>
2016-08-01 12:36:21 +0300

    communication-with-requests

--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
+// Modules Import
 import express from 'express';
+import bodyParser from 'body-parser';
+
+// Application's parts import
+import * as db from './utils/DataBaseUtils';
+
+// Modules applying
+db.setUpConnection();
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.use(bodyParser.json());
+
+// HTTP requests
+app.get('/notes', (req, res) => {
+	db.listNotes().then(data => res.send(data));
 });
+
+app.post('/notes', (req, res) => {
+	db.createNote(req.body).then(data => res.send(data));
+});
+
+app.delete('/notes/:id', (req, res) => {
+	db.deleteNote(req.params.id).then(data => res.send(data));
+});
+
+// Server Using
 const server = app.listen(8080, () => {
 	console.log(`Server is running`)
 });
